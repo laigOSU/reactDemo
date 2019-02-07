9cf0f29cf56ec2f6d55e3990d43a61c1b3879d77
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-02-07 00:21:24 -0800

    templateTwo

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,7 +1,16 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js!</p>;
+var template = (
+    <div>
+        <h1>This is JSX from app.js!</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
 // var template = React.createElement(
 //     "h1",
 //     {id: "someid"},
@@ -11,6 +20,24 @@ var template = <p>This is JSX from app.js!</p>;
 
 var appRoot = document.getElementById('app');
 
+var templateTwo = (
+    <div>
+        <h1>SUNNY</h1>
+        <p>Tengo seis anos</p>
+        <p>Soy de Guadalajara</p>
+    </div>
+
+);
+
+var appRootDos = document.getElementById('sunnyApp');
+// Create a templateTwo car JSX expression
+// div (root element)
+// h1 -> your name
+// p-> age: _
+// p-> location:
+// render tempalteTwo instaed of template
+
 // ReactDOM.render() takes 2 args: the JSX you'd like to render, and the elemn 
 // (where you'd like to render it)
 ReactDOM.render(template, appRoot); 
+ReactDOM.render(templateTwo, appRootDos); 
